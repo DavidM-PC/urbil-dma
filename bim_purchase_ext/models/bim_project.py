c4b59db4cfad99b0aa8b672071a89c0bc8956365
bitsluispaternina <89114780+lfpv1993@users.noreply.github.com>
2022-04-29 10:53:07 -0500

    add

--- bim_purchase_ext/models/bim_project.py
+++ bim_purchase_ext/models/bim_project.py
@@ -8,4 +8,15 @@ class BimProject(models.Model):
 
     purchase_line_ids = fields.Many2many(
         'purchase.order',
-        string="Compras")
+        string="Compras",
+        compute="compute_purchases")
+
+
+    def compute_purchases(self):
+    # Función para relacionar compras a obras
+        for record in self:
+            purchases = self.env['purchase.order'].search([('project_id','=',self.id)])
+            if purchases:
+                record.purchase_line_ids = purchases.ids
+            else:
+                record.purchase_line_ids = False
